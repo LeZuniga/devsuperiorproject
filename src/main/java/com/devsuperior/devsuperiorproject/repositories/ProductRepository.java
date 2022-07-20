c71a7d7722683ba03bf0133d9efd931e95418d3f
LeZuniga <leh.zuniga@gmail.com>
2022-07-20 00:01:55 -0300

    add anotações

--- src/main/java/com/devsuperior/devsuperiorproject/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/devsuperiorproject/repositories/ProductRepository.java
@@ -1,28 +1,12 @@
 package com.devsuperior.devsuperiorproject.repositories;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.persistence.Entity;
 
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.devsuperiorproject.entities.Product;
 
-@Component
-public class ProductRepository {
-    private Map<Long, Product> map = new  HashMap<>();
-
-	public void save(Product obj) {
-	map.put(obj.getId(), obj);
-	}
-
-	public Product findById(Long id) {
-		return map.get(id);
-	}
-	
-	public List<Product> findAll() {
-		return new ArrayList<Product>(map.values()); 
-	}
+@Entity
+public interface ProductRepository extends JpaRepository<Product, Long>{
 
 }
