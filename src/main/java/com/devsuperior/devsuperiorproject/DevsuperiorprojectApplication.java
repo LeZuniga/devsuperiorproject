f2915c6842ae9727f38adadc325996fbc0db4add
LeZuniga <leh.zuniga@gmail.com>
2022-07-19 22:17:11 -0300

    implementando produtos

--- src/main/java/com/devsuperior/devsuperiorproject/DevsuperiorprojectApplication.java
+++ src/main/java/com/devsuperior/devsuperiorproject/DevsuperiorprojectApplication.java
@@ -1,12 +1,16 @@
 package com.devsuperior.devsuperiorproject;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.devsuperiorproject.entities.Category;
+import com.devsuperior.devsuperiorproject.entities.Product;
 import com.devsuperior.devsuperiorproject.repositories.CategoryRepository;
+import com.devsuperior.devsuperiorproject.repositories.ProductRepository;
 
 @SpringBootApplication
 public class DevsuperiorprojectApplication implements CommandLineRunner{
@@ -14,17 +18,33 @@ public class DevsuperiorprojectApplication implements CommandLineRunner{
 	@Autowired
 	private CategoryRepository categoryRepository;
 
+	@Autowired
+	private ProductRepository productRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DevsuperiorprojectApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Category categoryId1 = new Category(1L, "Eletronics");
-		Category categoryId2 = new Category(2L, "Books");	
+		Category categoryId1 = new Category(1L, "Electronics");
+		Category categoryId2 = new Category(2L, "Books");
+		
+		Product p1 = new Product(1L, "TV", 2200.00, categoryId1);
+		Product p2 = new Product(2L, "Domain Driven Design", 120.00, categoryId2);
+		Product p3 = new Product(3L, "PS5", 2800.00, categoryId1);
+		Product p4 = new Product(4L, "Docker", 100.00, categoryId2);
+		
+		categoryId1.getProducts().addAll(Arrays.asList(p1, p3));
+		categoryId2.getProducts().addAll(Arrays.asList(p2, p4));
 
 		categoryRepository.save(categoryId1);
 		categoryRepository.save(categoryId2);
+
+		productRepository.save(p1);
+		productRepository.save(p2);
+		productRepository.save(p3);
+		productRepository.save(p4);
 	}
 
 }
---
+++ src/main/java/com/devsuperior/devsuperiorproject/resources/ProductResources.java
@@ -0,0 +1,34 @@
+package com.devsuperior.devsuperiorproject.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.devsuperiorproject.entities.Product;
+import com.devsuperior.devsuperiorproject.repositories.ProductRepository;
+    
+@RestController
+@RequestMapping (value="/products")
+public class ProductResources{
+    
+    @Autowired
+    private ProductRepository ProductRepository;
+
+    @GetMapping
+	public ResponseEntity<List<Product>> findAll() {
+        List<Product> list = ProductRepository.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+    @GetMapping(value = "/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id) {
+        Product ProductId = ProductRepository.findById(id);
+        return ResponseEntity.ok().body(ProductId);
+    }
+
+}
